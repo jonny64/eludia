bfd598ed32cd92c4ce5a1863af437491e0d6b4ea
Aleksandr Blinov <ablinov@cheby.ru>
2016-11-02 16:44:18 +0300

    contex_button_fix Исправлено формирование ссылок в контекстном меню

--- Eludia/Presentation/TableCells/button.pm
+++ Eludia/Presentation/TableCells/button.pm
@@ -12,8 +12,8 @@ sub draw_button_cell {
 		my $msg = js_escape ($options -> {confirm});
 		$options -> {href} =~ s{\%}{\%25}gsm; 		# wrong, but MSIE uri_unescapes the 1st arg of window.open :-(
 		my $target = $options -> {target} || '_self';
-		if ($options -> {href} =~ /^java/) {
-			$options -> {href} = qq [$$options{href}];
+		if ($options -> {href} =~ s/^javascript://g) {
+			$options -> {href} = qq [javascript:if (confirm ($msg)) {$$options{href}}];
 		}else{
 			$options -> {href} = qq [javascript:if (confirm ($msg)) {nope('$$options{href}', '$target')} else {document.body.style.cursor = 'default'; nop ();}];
 		}
